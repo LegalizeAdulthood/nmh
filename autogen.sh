0bb5b742a897926152c1791186e4cfc493fb89bf
Lyndon Nerenberg <lyndon@orthanc.ca>
2011-12-24 14:51:46 -0800

    Remove autoconf-related files.

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Regenerate config.h.in, configure, etc.
-# Necessary if building from CVS; not needed if
-# building from a distributed tarball.
-
-set -e
-autoreconf
-date > stamp-h.in
